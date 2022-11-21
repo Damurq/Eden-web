710c5eaf12f9e15966d5e603ab2cd7b0d6fe463a
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2022-11-20 23:24:26 -0400

    Tournament page - Events calendar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // Third party
-import React                                      from "react"
-import { BrowserRouter as Router, Switch, Route  } from "react-router-dom"
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 // Local
-import Navbar           from "./components/Navbar/Navbar"
-import Footer           from "./components/Footer/Footer"
-import Home             from "./pages/Home/Home"
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
+import Home from "./pages/Home/Home"
+import Events from "./pages/Events/Events"
+import Tournament from "./pages/Tournament/Tournament"
 // Data
-import data             from "./data/components.json"
+import data from "./data/components.json"
 // Styles
 import "./App.css"
 
@@ -14,14 +16,20 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar data={data.Navbar} />
+        <Navbar data={data.Navbar} />
         <Switch>
+          <Route path="/Eventos">
+            <Events />
+          </Route>
+          <Route path="/Torneos">
+            <Tournament />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
         <Footer data={data.Footer} />
-      </Router>      
+      </Router>
     </div>
   );
 }
