a073cef5f09449d32f84f0866d968c3e788007ee
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2023-01-23 17:44:58 -0400

    update

--- src/components/InitialCard/InitialCard.js
+++ src/components/InitialCard/InitialCard.js
@@ -4,12 +4,10 @@ function InitialCard() {
 
   return (
     <div className='portada'>
-
       <div className='TextInitialCard'>
         <sub className='Text1'>El mejor club de Barquisimeto</sub>
         <h1 className='Text2'>Disfruta de una experiencia increible en Apucla</h1>
       </div>
-
       <div className='Frame29'>
         <h2 className='Text3'>Siguenos-</h2>
       </div>
