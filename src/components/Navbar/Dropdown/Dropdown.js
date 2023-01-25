41a8217107e8daa5bb106ffddfdaee43c1a884f7
Luis Torrealba <luisdavid25torrealba@gmail.com>
2023-01-25 01:03:54 -0400

    actualización del navbar

--- src/components/Navbar/Dropdown/Dropdown.js
+++ src/components/Navbar/Dropdown/Dropdown.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { MenuItems } from './MenuItems';
 import './Dropdown.css';
 import { Link } from 'react-router-dom';
 
-function Dropdown() {
+//dataDropdown
+import { SubMenuAboutUs, SubMenuEvents, SubMenuActivities } from './MenuItems';
+
+function Dropdown({subMenu}) {
+
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
@@ -13,19 +16,64 @@ function Dropdown() {
         onClick={handleClick}
         className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}
       >
-        {MenuItems.map((item, index) => {
-          return (
-            <li key={index}>
-              <Link
-                className={item.cName}
-                to={item.path}
-                onClick={() => setClick(false)}
-              >
-                {item.title}
-              </Link>
-            </li>
-          );
-        })}
+        {
+            (subMenu == "Sobre nosotros" )
+             &&
+              <>
+              {SubMenuAboutUs.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <a
+                      className={item.cName}
+                      href={item.path}
+                      onClick={() => setClick(false)}
+                    >
+                      {item.title}
+                    </a>
+                  </li>
+                );
+              })}
+             </>
+        }
+        {
+            (subMenu == "Eventos" )
+             &&
+              <>
+              {SubMenuEvents.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <a
+                      className={item.cName}
+                      href={item.path}
+                      onClick={() => setClick(false)}
+                    >
+                      {item.title}
+                    </a>
+                  </li>
+                );
+              })}
+             </>
+        }
+        {
+            (subMenu == "Actividades" )
+             &&
+              <>
+              {SubMenuActivities.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <a
+                      className={item.cName}
+                      href={item.path}
+                      onClick={() => setClick(false)}
+                    >
+                      {item.title}
+                    </a>
+                  </li>
+                );
+              })}
+             </>
+        }
+        
       </ul>
   );
 }
