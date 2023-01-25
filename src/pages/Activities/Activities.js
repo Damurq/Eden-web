104834f1ac11c4a68f42cc4d9fc1c7e485dd9714
MarihecMiranda <marihecmiranda160498@gmail.com>
2023-01-25 00:27:38 -0400

    Actividades

---
+++ src/pages/Activities/Activities.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ActivitiesCalendar from '../../components/ActivityCalendar/ActivityCalendar';
+
+const Activities = () => {
+  return (
+    <div className="Content" style={{height:"650px"}}>
+        <ActivitiesCalendar/>
+    </div>
+  )
+}
+
+export default Activities;
