83c3ab53b73658077204a812a8350b7f7510dbc7
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2023-01-26 18:01:40 -0400

    refactorizar el codigo de torneos y sus secciones

--- src/pages/Results/Results.js
+++ src/pages/Results/Results.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
+// Components
+import MatchGroup from '../../components/specific/MatchGroup/MatchGroup'
+// Data
 import data from "../../data/Tournaments.json"
 import { weekResults } from '../../data/weekResults'
-import MatchGroup from '../../components/MatchGroup/MatchGroup'
+// Styles
 import './Results.css'
 
 const Results = () => {
@@ -13,10 +16,8 @@ const Results = () => {
             <div className="information-elements section">
                 {sections && sections.map((section, i) => (
                     <div key={`${data[section].title}-${i}`} className="info-element--container" >
-                        <div className={`custom-button info-element ${activeSection === section
-                            ? "active-element"
-                            : ""
-                            }`}
+                        <div
+                            className={`custom-button info-element ${activeSection === section ? "active-element" : ""}`}
                             id={section}
                             onClick={e => {
                                 setActiveSection(e.currentTarget.id)
@@ -28,13 +29,13 @@ const Results = () => {
             </div>
 
             {weekResults[activeSection]
-                ? <div className="results-container">
+                ? (<div className="results-container">
                     <h2 className="results-container__title">Resultados</h2>
                     {weekResults[activeSection].map((item, index) => <MatchGroup
                         tournament={item.tournament}
                         data={item.data}
                         key={`tournament-${index}`} />)}
-                </div>
+                </div>)
                 : ""}
         </div>
     )
