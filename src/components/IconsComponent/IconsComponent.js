144dcc69286d4d55ea1478f72bd6ee3a2250021c
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2023-01-26 17:34:01 -0400

    update refactor

--- src/components/IconsComponent/IconsComponent.js
+++ src/components/IconsComponent/IconsComponent.js
@@ -1,37 +1,23 @@
 import React                                                                    from 'react'
-import { AiOutlineAntDesign, AiOutlineProfile, AiOutlineHome, AiOutlineSend }   from 'react-icons/ai'
-import { BiUser, BiBriefcase }                                                  from 'react-icons/bi'
-import { BsBraces, BsFillBriefcaseFill, BsTelephone }                           from 'react-icons/bs'
-import { FaGraduationCap, FaPuzzlePiece, FaUniversity, FaPython }               from 'react-icons/fa'
-import { FiDatabase }                                                           from 'react-icons/fi'
+import { AiOutlineSend }                                                        from 'react-icons/ai'
+import { BsTelephone, BsInstagram, BsFacebook }                                 from 'react-icons/bs'
+import { FaLinkedinIn }                                                         from 'react-icons/fa'
 import { MdOutlineCollections }                                                 from 'react-icons/md'
 import { HiOutlineMail, HiOutlineLocationMarker }                               from 'react-icons/hi'
 import { MdOutlineWeb }                                                         from 'react-icons/md'
-import { SiMaterialdesignicons, SiAltiumdesigner }                              from 'react-icons/si'
-import { DiJavascript1 }                                                        from 'react-icons/di'
+import { SiAltiumdesigner }                                                     from 'react-icons/si'
+
 
 const IconsComponent = ({ icon, className = '' }) => {
 
     const selectIcon = () => {
         let element = <div></div>
         switch (icon) {
-            case 'brackets':
-                element = <BsBraces className={className} />
+            case 'instagram':
+                element = <BsInstagram className={className} />
                 break;
-            case 'database':
-                element = <FiDatabase className={className} />
-                break;
-            case 'education':
-                element = <FaGraduationCap className={className} />
-                break;
-            case 'work':
-                element = <BsFillBriefcaseFill className={className} />
-                break;
-            case 'frontend':
-                element = <MdOutlineWeb className={className} />
-                break;
-            case 'design':
-                element = <AiOutlineAntDesign className={className} />
+            case 'facebook':
+                element = <BsFacebook className={className} />
                 break;
             case 'phone':
                 element = <BsTelephone className={className} />
@@ -42,42 +28,18 @@ const IconsComponent = ({ icon, className = '' }) => {
             case 'email':
                 element = <HiOutlineMail className={className} />
                 break;
-            case 'home':
-                element = <AiOutlineHome className={className} />
-                break;
-            case 'user':
-                element = <BiUser className={className} />
-                break;
-            case 'skills':
-                element = <AiOutlineProfile className={className} />
-                break;
-            case 'services':
-                element = <BiBriefcase className={className} />
-                break;
             case 'portfolio':
                 element = <MdOutlineCollections className={className} />
                 break;
             case 'send':
                 element = <AiOutlineSend className={className} />
                 break;
-            case 'structure':
-                element = <SiMaterialdesignicons className={className} />
-                break;
             case 'area':
                 element = <SiAltiumdesigner className={className} />
                 break;    
-            case 'puzzle':
-                element = <FaPuzzlePiece className={className} />
-                break;  
-            case 'university':
-                element = <FaUniversity className={className} />
-                break;    
-            case 'js':
-                element = <DiJavascript1 className={className} />
-                break;    
-            case 'python':
-                element = <FaPython className={className} />
-                break;  
+            case 'linkedin':
+                element = <FaLinkedinIn className={className} />
+                break;      
             default:
                 element = <MdOutlineWeb className={className} />
                 break;
