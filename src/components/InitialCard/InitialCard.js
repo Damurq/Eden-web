e35db3394d1d9cbe65ed34d92a16815f9ff71aa6
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2023-02-03 19:57:26 -0400

    navbar and initial component

--- src/components/InitialCard/InitialCard.js
+++ src/components/InitialCard/InitialCard.js
@@ -1,28 +1,45 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+import SocialMedia from "../specific/SocialMedia/SocialMedia"
+import socialMedia from "../../data/socialMedia.json"
 import './InitialCard.css';
 
-function InitialCard({data}) {
+function InitialCard() {
   let imgUrl = './InitialCard/background.png'
-  const portadaStyle = {
+
+  const [data, setData] = useState({})
+  const [portadaStyle, setPortadaStyle] = useState({
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center",
-    backgroundImage: 'url(' + imgUrl + ')'
-  };
+    backgroundColor: "#7e7d7d5e"
+  })
+
+  useEffect(() => {
+    setData({
+      imgUrl,
+      title: 'El mejor club de Barquisimeto',
+      subtitle: 'Disfruta de una experiencia increible en Apucla'
+    })
+    setPortadaStyle({ ...portadaStyle, backgroundImage: 'url(' + imgUrl + ')' })
+  }, [])
 
   return (
     <div className='portada' style={portadaStyle}>
-      <div className='TextInitialCard'>
-        {<sub className='Text1'>El mejor club de Barquisimeto</sub>}
-        {<h1 className='Text2'>Disfruta de una experiencia increible en Apucla</h1>}
+      <div className='portada-text-container'>
+        <sub className='portada-text-container__title'>{data.title || <Skeleton />}</sub>
+        <h1 className='portada-text-container__subtitle'>{data.subtitle || <Skeleton />}</h1>
       </div>
-      <div className='Frame29'>
-        {<h2 className='Text3'>Siguenos-</h2>}
+      <div className='portada-social-media'>
+        <h2 className='portada-social-media__text'>Siguenos-</h2>
+        {socialMedia ? <SocialMedia data={socialMedia} componentName="InitialCard" orientation="vertical" /> : <Skeleton />}
       </div>
       <div className='button-container'>
-      <button className='Group2 button-yellow'>
-        Explorar
-      </button>
+        <button className='portada__button button-action'>
+          Explorar
+        </button>
       </div>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,13 @@
-import React            from "react"
+import React, { useEffect , useState } from "react"
 // Components
 import CarruselDiscover from "../../components/CarruselDiscover/CarruselDiscover"
-import CardContact      from "../../components/CardContact/CardContact.js"
-import Presentation      from "../../components/Presentation/Presentation.js"
-import Tournament      from "../../components/Tournament/Tournament.js"
-import InitialCard      from "../../components/InitialCard/InitialCard.js"
-import data             from "../../data/Home.json"
+import CardContact from "../../components/CardContact/CardContact.js"
+import Presentation from "../../components/Presentation/Presentation.js"
+import Tournament from "../../components/Tournament/Tournament.js"
+import InitialCard from "../../components/InitialCard/InitialCard.js"
+import data from "../../data/Home.json"
 
-const Home = ()=>{
+const Home = () => {
 
     return (
         <div className="Content">
