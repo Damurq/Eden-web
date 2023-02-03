758ddccd83f4bc93b9178ba8856073162ac96751
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2023-02-03 17:57:02 -0400

    update

--- src/components/InitialCard/InitialCard.js
+++ src/components/InitialCard/InitialCard.js
@@ -1,15 +1,23 @@
+import React, { useState } from "react"
 import './InitialCard.css';
 
-function InitialCard() {
+function InitialCard({data}) {
+  let imgUrl = './InitialCard/background.png'
+  const portadaStyle = {
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundImage: 'url(' + imgUrl + ')'
+  };
 
   return (
-    <div className='portada'>
+    <div className='portada' style={portadaStyle}>
       <div className='TextInitialCard'>
-        <sub className='Text1'>El mejor club de Barquisimeto</sub>
-        <h1 className='Text2'>Disfruta de una experiencia increible en Apucla</h1>
+        {<sub className='Text1'>El mejor club de Barquisimeto</sub>}
+        {<h1 className='Text2'>Disfruta de una experiencia increible en Apucla</h1>}
       </div>
       <div className='Frame29'>
-        <h2 className='Text3'>Siguenos-</h2>
+        {<h2 className='Text3'>Siguenos-</h2>}
       </div>
       <div className='button-container'>
       <button className='Group2 button-yellow'>
