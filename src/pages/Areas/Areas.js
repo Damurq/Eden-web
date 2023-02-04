a1f8f1dbdd3661c16c4d0b9a713c86820f015e8d
Luis Torrealba <luisdavid25torrealba@gmail.com>
2023-02-03 22:27:31 -0400

    Recursos Disponibles

--- src/pages/Areas/Areas.js
+++ src/pages/Areas/Areas.js
@@ -1,11 +1,13 @@
 import React from 'react'
 // Components
 import SliderSyncing from "../../components/CarruselDiscover/SliderSyncing"
+import {Resources} from "../../components/CarruselDiscover/Resources"
 
 const Areas = () => {
   return (
     <div className="Content">
         <SliderSyncing/>
+        <Resources/>
     </div>
   )
 }
