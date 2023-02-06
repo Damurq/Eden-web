d1d3a2b765eecff210ed35d566e2edc4ffeefbf7
Luis Torrealba <luisdavid25torrealba@gmail.com>
2023-02-06 01:08:20 -0400

    actualizacion de home, carrusel Actividades y eventos conectados al backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const AREAS = "areas"
 const INSTALACIONES = "instalaciones"
+const EVENTOS = "eventos"
+const ACTIVIDADES = "actividades"
 
 module.exports = {
     AREAS,
-    INSTALACIONES
+    INSTALACIONES,
+    EVENTOS,
+    ACTIVIDADES
 }
