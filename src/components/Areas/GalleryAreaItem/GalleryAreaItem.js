70a5e8e705f7c102fc15ad9046e62eaa1d570527
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2023-02-06 12:16:28 -0400

    update dates

--- src/components/Areas/GalleryAreaItem/GalleryAreaItem.js
+++ src/components/Areas/GalleryAreaItem/GalleryAreaItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Glider from 'react-glider';
 import { Link } from 'react-router-dom';
+
+import SeeMore from '../../SeeMore/SeeMore';
 //Estilos y diseño
 import './GalleryAreaItem.css';
 import '../Resources/Resources.css';
@@ -37,54 +39,55 @@ const GalleryAreaItem = (props) => {
         </div>
         {/* ----------- Instalaciones Disponibles ----------------*/}
         {
-          props.conditions == "GalleryArea" ? (
-            <> 
-            <div className='Installation__main__container' >
-              <div className='Installation__cont'>
-                {/*  Titulo de Instalaciones  */}
-                <div className="Title__carousel__start">
-                  <h2>Instalaciones</h2>
-                </div>
-                {/*  Lista  de Instalaciones  */}
-                <div className='Installation__List'>
-                  <div className='Installation__container'>
-                    <Glider
-                      draggable
-                      hasDots
-                      scrollLock
-                      slidesToShow={1}
-                      slidesToScroll={1}
-                      responsive={[
-                        {
-                          breakpoint: 960,
-                          settings: {
-                            slidesToShow: 3,
-                          },
-                        },
-                      ]}
-                    >
-                      {props.installation.map((item, index) => (
-                        <div key={`${props.component}-${index}-${item.id}`} className='Installation__Item'>
-                          <Link className='Installation__Item__Link' to={`/InstallationDetails/${item.id}`}>
-                            <img
-                              className='Installation__Item__img'
-                              alt={item.nombre}
-                              src={item.imagen_principal} />
-                            <div className='cards__item__info-two'>
-                              <h5 className='cards__item__text-two'>{item.nombre}</h5>
+          props.conditions === "GalleryArea"
+            ? (
+              <>
+                <div className='Installation__main__container' >
+                  <div className='Installation__cont'>
+                    {/*  Titulo de Instalaciones  */}
+                    <div className="Title__carousel__start">
+                      <h2>Instalaciones</h2>
+                    </div>
+                    {/*  Lista  de Instalaciones  */}
+                    <div className='Installation__List'>
+                      <div className='Installation__container'>
+                        <Glider
+                          draggable
+                          hasDots
+                          scrollLock
+                          slidesToShow={1}
+                          slidesToScroll={1}
+                          responsive={[
+                            {
+                              breakpoint: 960,
+                              settings: {
+                                slidesToShow: 3,
+                              },
+                            },
+                          ]}
+                        >
+                          {props.installation.map((item, index) => (
+                            <div key={`${props.component}-${index}-${item.id}`} className='Installation__Item'>
+                              <Link className='Installation__Item__Link' to={`/InstallationDetails/${item.id}`}>
+                                <img
+                                  className='Installation__Item__img'
+                                  alt={item.nombre}
+                                  src={item.imagen_principal} />
+                                <div className='cards__item__info-two'>
+                                  <h5 className='cards__item__text-two'>{item.nombre}</h5>
+                                </div>
+                              </Link>
                             </div>
-                          </Link>
-                        </div>
-                      ))}
-                    </Glider>
+                          ))}
+                        </Glider>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            </>
-          ):null
+              </>
+            )
+            : <SeeMore to={"/Areas"} />
         }
-        
       </div>
     </>
   )
