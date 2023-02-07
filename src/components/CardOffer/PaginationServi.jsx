113e8274ae10e6aaf5b08dccc9bca0cbbe6f3755
orianansant <oriannasantana0511@hotmail.com>
2023-02-07 10:05:57 -0400

    se hizo la paginacion

---
+++ src/components/CardOffer/PaginationServi.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './PaginationServi.css';
+
+const PaginationServi = ({ servicePerPage, totalService, currentPage, setCurrentPage}) => {
+
+    const pageNumbers = []
+   
+    for(let i = 1; i <= Math.ceil(totalService/servicePerPage); i++){
+        pageNumbers.push(i)
+    }
+    
+    const onPreviusPage = ()=> {
+        setCurrentPage(currentPage -1)
+    }
+    const onNextPage = ()=> {
+        setCurrentPage(currentPage +1)
+    }
+    const onSpecifiPage = (n)=> {
+        setCurrentPage(n)
+    }
+
+
+    return(
+        <div className='conteiner-pag'> 
+            <div className="pagination" >
+                 
+                 <a  onClick={onPreviusPage}>Previo</a>
+                
+  
+                            
+                                {
+                                    pageNumbers.map(noPage => (
+                                        <a key={noPage}>
+                                            <a className={`pagination-link ${noPage === currentPage ? 'is-current' : ''}`} onClick = { ()  => onSpecifiPage(noPage)}>
+                                            {noPage}
+                                            </a>
+                                        </a>
+                                    )
+
+                                    )
+                                }
+                            
+                <a  onClick={onNextPage} >Siguiente</a>      
+            </div>
+            </div>
+            )
+    
+}
+export default PaginationServi
