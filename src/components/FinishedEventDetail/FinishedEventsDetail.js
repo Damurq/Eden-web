f723838b94dac8e4b1e97b84e15d68bfdb5854ab
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2023-02-07 21:37:26 -0400

    corregir errores pequeños y validaciones

--- src/components/FinishedEventDetail/FinishedEventsDetail.js
+++ src/components/FinishedEventDetail/FinishedEventsDetail.js
@@ -9,9 +9,9 @@ import Comment from "../Comment/Comment"
 
 
 // Data
-import {EVENTO_DETALLE} from '../../routes/index'
+import { EVENTO } from '../../routes/index'
 
-const Gallery = ({ component = "undefined" }) =>  {
+const Gallery = ({ component = "undefined" }) => {
 
     const { id } = useParams();
     const [finishedEvent, setFinishedEvent] = useState({});
@@ -19,8 +19,9 @@ const Gallery = ({ component = "undefined" }) =>  {
 
     useEffect(() => {
         const getFinishedEvent = async (id) => {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}${EVENTO_DETALLE}/${id}`);
+            const response = await fetch(`${process.env.REACT_APP_API_URL}${EVENTO}${id}`);
             const res = await response.json();
+            console.log(res.data);
             setFinishedEvent(res.data);
         }
 
@@ -29,112 +30,116 @@ const Gallery = ({ component = "undefined" }) =>  {
 
     /*console.log(finishedEvent);*/
 
-    
+
     const filteredComments = finishedEvent.comentarios ? finishedEvent.comentarios.filter(comment => {
-    const createdCommentDate = new Date(finishedEvent.comentarios.creado);
-    const finishEventDate = new Date(finishedEvent.fecha_finalizacion);
-    return createdCommentDate >= finishEventDate;
+        const createdCommentDate = new Date(finishedEvent.comentarios.creado);
+        const finishEventDate = new Date(finishedEvent.fecha_finalizacion);
+        return createdCommentDate >= finishEventDate;
     }) : [];
-  
+
     return (
-        
+
         <div className="Gallery-event">
             <div>
-                
-                    <div>
-                        <div className="Title-carousel-event">
-                            <h1>{finishedEvent.nombre || ""}</h1>
-                        </div>
 
-                        <div className="Carousel-Gallery-event">
-                            
+                <div>
+                    <div className="Title-carousel-event">
+                        <h1>{finishedEvent.nombre || ""}</h1>
+                    </div>
 
-                                
-                            <Carousel className="Carousel-Gallery-event" autoPlay interval="5000" transitionTime="500" infiniteLoop style={{ with: '50%' }}>
+                    <div className="Carousel-Gallery-event">
 
-                                {finishedEvent.galeria ? finishedEvent.galeria.map((gal) => {
 
-                                    return(
-                                
-                                        <div key={`${gal.id}-imagen`} className={`"events-list"${gal.id}`}>
-                                            <img src={gal.imagen} alt="" style={{ width: "100%", objectFit: "cover", borderRadius: "10px" }} />
-                                            
-                                        
-                                        </div>
-                                    )
 
-                                }): <div>Loading...</div>}
-                                    
-                                        
-                            
-                            </Carousel>
-                                
-                                
-                        </div>
+                        {(finishedEvent?.galeria?.lenght) ? <Carousel className="Carousel-Gallery-event" autoPlay interval="5000" transitionTime="500" infiniteLoop style={{ with: '50%' }}>
 
-                        
+                            {finishedEvent?.galeria ? finishedEvent.galeria.map((gal) => {
 
-                        <div className="container__comentary__section">
-                            <div className="comments__section">
-                                <div className='comments__title'>
-                                    <h1>Comentarios</h1>
-                                </div>
+                                return (
 
-                                
-                                <div className="comments-event-list">
-                                    
-                                    {finishedEvent.comentarios ? finishedEvent.comentarios.map((comment) => {
+                                    <div key={`${gal.id}-imagen`} className={`"events-list"${gal.id}`}>
+                                        <img src={gal.imagen} alt="" style={{ width: "100%", objectFit: "cover", borderRadius: "10px" }} />
 
-                                        let created_date = new Date(comment.creado);
-                                        
 
-                                        return(
-                                            <div key={`${comment.id}-event`} className={`comments-event-element${comment.id === filteredComments
-                                                ? "active-event"
-                                                : ""}`}>
-                                    
-                                                <div className='comment__up'>
-                                                    <div className='comment__user'>
-                                                        <h2>
-                                                            {comment.nombre}, {comment.edad} años.
-                                                        </h2>
-                                                    </div>
-                                                    <div className='comment__date'>
-                                                        <span>
-                                                            {created_date.toLocaleDateString()}
-                                                        </span>
-                                                    </div>
+                                    </div>
+                                )
+
+                            }) : <div>Loading...</div>}
+
+
+
+                        </Carousel>
+                            : (finishedEvent?.imagen_principal) ? <div className='Content-img'>
+                                <img src={finishedEvent?.imagen_principal} alt={finishedEvent?.nombre} />
+                            </div>
+                                : null
+                        }
+
+                    </div>
+
+
+
+                    <div className="container__comentary__section">
+                        <div className="comments__section">
+                            <div className='comments__title'>
+                                <h1>Comentarios</h1>
+                            </div>
+
+
+                            <div className="comments-event-list">
+
+                                {finishedEvent.comentarios ? finishedEvent.comentarios.map((comment) => {
+
+                                    let created_date = new Date(comment.creado);
+
+
+                                    return (
+                                        <div key={`${comment.id}-event`} className={`comments-event-element${comment.id === filteredComments
+                                            ? "active-event"
+                                            : ""}`}>
+
+                                            <div className='comment__up'>
+                                                <div className='comment__user'>
+                                                    <h2>
+                                                        {comment.nombre}, {comment.edad} años.
+                                                    </h2>
                                                 </div>
-                                                <div className='comment__down'>
-                                                    <p>
-                                                        {comment.comentario}
-                                                    </p>
+                                                <div className='comment__date'>
+                                                    <span>
+                                                        {created_date.toLocaleDateString()}
+                                                    </span>
                                                 </div>
-                                            
-                    
+                                            </div>
+                                            <div className='comment__down'>
+                                                <p>
+                                                    {comment.comentario}
+                                                </p>
                                             </div>
 
-                                        )
-                                    }): <div>Loading...</div>}
-                                </div>
-                                
-                                
-                                
-                            </div>
-                            <div className="do-comment">
-                                <Comment />
-                            </div>
-                        </div>
 
-                        
+                                        </div>
+
+                                    )
+                                }) : <div>Loading...</div>}
+                            </div>
+
+
+
+                        </div>
+                        <div className="do-comment">
+                            <Comment />
+                        </div>
                     </div>
-                
+
+
+                </div>
+
             </div>
         </div>
 
 
-                        
-)
-    
+
+    )
+
 }
 export default Gallery
